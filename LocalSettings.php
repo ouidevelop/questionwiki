dc56ba6cf071419df41cb11488ef08998762255b
Michael Kurrels <mjkurrels@gmail.com>
2021-06-29 16:03:02 -0700

    restrict editing

--- LocalSettings.php
+++ LocalSettings.php
@@ -226,6 +226,8 @@ $wgSMTP = [
 
 // comment this line out when you want people to be able to create accounts
 $wgGroupPermissions['*']['createaccount'] = false;
+// restrict editing
+$wgGroupPermissions['user']['edit'] = false;
 
 wfLoadExtension( 'UserMerge' );
 // By default nobody can use this function, enable for bureaucrat?
