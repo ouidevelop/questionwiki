0ec5c11aa7c31271c66d1094fea19d9bb09ec4e3
Michael Kurrels <mjkurrels@gmail.com>
2021-08-28 13:07:16 -0700

    removes logo

--- LocalSettings.php
+++ LocalSettings.php
@@ -48,7 +48,7 @@ $wgResourceBasePath = $wgScriptPath;
 
 ## The URL paths to the logo.  Make sure you change this from the default,
 ## or else you'll overwrite your logo when you upgrade!
-$wgLogos = [ '1x' => "$wgResourceBasePath/resources/assets/wiki.png" ];
+// $wgLogos = [ '1x' => "$wgResourceBasePath/resources/assets/wiki.png" ];
 
 ## UPO means: this is also a user preference option
 
